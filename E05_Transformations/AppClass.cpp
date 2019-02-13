e261ec9985f69a353bf82d274316ea5add7ed4d3
Josh Richards <jmr8776@rit.edu>
2019-02-12 19:39:21 -0500

    E05 Deliverable

--- E05_Transformations/AppClass.cpp
+++ E05_Transformations/AppClass.cpp
@@ -3,8 +3,7 @@ void Application::InitVariables(void)
 {
 	//init the mesh
 	m_pMesh = new MyMesh();
-	//m_pMesh->GenerateCube(1.0f, C_WHITE);
-	m_pMesh->GenerateSphere(1.0f, 5, C_WHITE);
+	m_pMesh->GenerateCube(1.0f, C_BLACK);
 }
 void Application::Update(void)
 {
@@ -25,15 +24,33 @@ void Application::Display(void)
 	matrix4 m4View = m_pCameraMngr->GetViewMatrix();
 	matrix4 m4Projection = m_pCameraMngr->GetProjectionMatrix();
 	
-	matrix4 m4Scale = glm::scale(IDENTITY_M4, vector3(2.0f,2.0f,2.0f));
+	matrix4 m4Scale = glm::scale(IDENTITY_M4, vector3(.5f, .5f, .5f));
 	static float value = 0.0f;
-	matrix4 m4Translate = glm::translate(IDENTITY_M4, vector3(value, 2.0f, 3.0f));
+
+	std::vector< std::vector <int > > ImageData = 
+	{
+		{2,8},
+		{3,7},
+		{2,3,4,5,6,7,8},
+		{1,2,4,5,6,8,9},
+		{0,1,2,3,4,5,6,7,8,9,10},
+		{0,2,3,4,5,6,7,8,10},
+		{0,2,8,10},
+		{3,4,6,7}
+	};
+
+	for (int i = 0; i < ImageData.size(); ++i) 
+	{
+		for (int j = 0; j < ImageData[i].size(); ++j)
+		{
+			matrix4 m4Translate = glm::translate(IDENTITY_M4, vector3(ImageData[i][j] + value - 10.0f, ImageData.size() - i, 0.0f));
+			//matrix4 m4Model = m4Translate * m4Scale;
+			matrix4 m4Model = m4Scale * m4Translate;
+
+			m_pMesh->Render(m4Projection, m4View, m4Model);
+		}
+	}
 	value += 0.01f;
-
-	//matrix4 m4Model = m4Translate * m4Scale;
-	matrix4 m4Model = m4Scale * m4Translate;
-
-	m_pMesh->Render(m4Projection, m4View, m4Model);
 	
 	// draw a skybox
 	m_pMeshMngr->AddSkyboxToRenderList();
